c6ea72e2eae17dc6fdb405b0ef3184d6299c7e23
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-10-04 22:03:19 +0100

    feat: integrate MySQL, MariaDB, MongoDB, Redis into main SDK

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,10 @@ import { ComposeApi } from './api/compose';
 import { DockerApi } from './api/docker';
 import { DomainApi } from './api/domain';
 import { PostgresApi } from './api/postgres';
+import { MySqlApi } from './api/mysql';
+import { MariadbApi } from './api/mariadb';
+import { MongoApi } from './api/mongo';
+import { RedisApi } from './api/redis';
 
 export class DokploySDK {
   public admin: AdminApi;
@@ -20,6 +24,10 @@ export class DokploySDK {
   public docker: DockerApi;
   public domain: DomainApi;
   public postgres: PostgresApi;
+  public mysql: MySqlApi;
+  public mariadb: MariadbApi;
+  public mongo: MongoApi;
+  public redis: RedisApi;
 
   constructor(config: DokployConfig) {
     const httpClient = new HttpClient(config);
@@ -32,6 +40,10 @@ export class DokploySDK {
     this.docker = new DockerApi(httpClient);
     this.domain = new DomainApi(httpClient);
     this.postgres = new PostgresApi(httpClient);
+    this.mysql = new MySqlApi(httpClient);
+    this.mariadb = new MariadbApi(httpClient);
+    this.mongo = new MongoApi(httpClient);
+    this.redis = new RedisApi(httpClient);
   }
 }
 
@@ -45,6 +57,10 @@ export * from './types/compose';
 export * from './types/docker';
 export * from './types/domain';
 export * from './types/postgres';
+export * from './types/mysql';
+export * from './types/mariadb';
+export * from './types/mongo';
+export * from './types/redis';
 export { AdminApi } from './api/admin';
 export { ApplicationApi } from './api/application';
 export { AuthApi } from './api/auth';
@@ -54,3 +70,7 @@ export { ComposeApi } from './api/compose';
 export { DockerApi } from './api/docker';
 export { DomainApi } from './api/domain';
 export { PostgresApi } from './api/postgres';
+export { MySqlApi } from './api/mysql';
+export { MariadbApi } from './api/mariadb';
+export { MongoApi } from './api/mongo';
+export { RedisApi } from './api/redis';
