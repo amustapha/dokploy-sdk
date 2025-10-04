e772d579092917c60b688fb5c9f6f3135c5d6f66
Abdulhakeem Mustapha <abdulhakeemmustapha@gmail.com>
2025-10-04 21:17:29 +0100

    feat: add HTTP client with authentication support

---
+++ src/client/http-client.ts
@@ -0,0 +1,50 @@
+import axios, { AxiosInstance, AxiosError } from 'axios';
+import { DokployConfig, ApiError } from '../types/common';
+
+export class HttpClient {
+  private client: AxiosInstance;
+
+  constructor(config: DokployConfig) {
+    this.client = axios.create({
+      baseURL: config.baseUrl,
+      headers: {
+        Authorization: `Bearer ${config.token}`,
+        'Content-Type': 'application/json',
+      },
+    });
+  }
+
+  async get<T>(endpoint: string, params?: Record<string, unknown>): Promise<T> {
+    try {
+      const response = await this.client.get<T>(endpoint, { params });
+      return response.data;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+
+  async post<T>(
+    endpoint: string,
+    data?: Record<string, unknown>
+  ): Promise<T> {
+    try {
+      const response = await this.client.post<T>(endpoint, data);
+      return response.data;
+    } catch (error) {
+      throw this.handleError(error);
+    }
+  }
+
+  private handleError(error: unknown): ApiError {
+    if (axios.isAxiosError(error)) {
+      const axiosError = error as AxiosError;
+      return {
+        message: axiosError.message,
+        statusCode: axiosError.response?.status,
+      };
+    }
+    return {
+      message: 'An unknown error occurred',
+    };
+  }
+}
